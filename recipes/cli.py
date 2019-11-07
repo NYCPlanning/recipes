26a15015466530c6ed25185dd3865fb69e6f5878
sptkl <caobaiyue@gmail.com>
2019-11-07 19:06:58 +0000

    adding dpr_capitalprojects

--- recipes/cli.py
+++ recipes/cli.py
@@ -13,7 +13,8 @@ CONFIG_PATH = os.path.join(
     'recipe_info'
 )
 
-special_recipes = ['pluto_input_cama_dof', 'pluto_pts', 'pluto_input_geocodes']
+sh_recipes = ['pluto_input_cama_dof', 'pluto_pts', 'pluto_input_geocodes']
+py_recipes = ['dpr_capitalprojects']
 
 @click.group()
 def cli():
@@ -31,33 +32,35 @@ def load_recipe_json(recipe):
     
     return recipe_config
 
-@cli.command('run')
-@click.argument('recipe', type=click.STRING, autocompletion=get_recipes)
-def run_recipes(recipe):
-    try:
-        if recipe in special_recipes:
-            os.system(f'bash {Path(__file__).parent}/recipe_info/{recipe}.sh')
-        else:
-            # os.system('cook convert')
-            recipe_config = load_recipe_json(recipe)
-            archiver = Archiver(engine=os.environ['RECIPE_ENGINE'], 
-                                ftp_prefix=os.environ['FTP_PREFIX'])
-            archiver.archive_table(recipe_config)
-    except KeyError: 
-        click.secho('\n Did you set your RECIPE_ENGINE and FTP_PREFIX? \n', fg='red')
-
-@cli.command('convert')
-def convert_recipes():
+def convert_recipe(recipe):
     df=pd.read_csv(Path(__file__).parent/'recipes.csv')
+    df=df[df.schema_name == recipe]
     df.loc[:,'layerCreationOptions'] = df.loc[:,'layerCreationOptions'].apply(lambda x: x if pd.isna(x) else literal_eval(x))
     df.loc[:,'srcOpenOptions'] = df.loc[:,'srcOpenOptions'].apply(lambda x: x if pd.isna(x) else literal_eval(x))
     df.loc[:,'newFieldNames'] = df.loc[:,'newFieldNames'].apply(lambda x: x if pd.isna(x) else literal_eval(x))
     df = df.replace(np.nan, '', regex=True)
     for row in df.iterrows():
         recipe = dict(row[1])
-        if recipe in special_recipes:
+        if recipe in py_recipes+sh_recipes:
             pass
         else:
             print(f"converting {recipe['schema_name']} ...")
             with open(f"{Path(__file__).parent/'recipe_info'/recipe['schema_name']}.json", 'w') as recipe_json:
                 json.dump(recipe, recipe_json, indent=4, ensure_ascii=False)
+
+@cli.command('run')
+@click.argument('recipe', type=click.STRING, autocompletion=get_recipes)
+def run_recipes(recipe):
+    try:
+        if recipe in sh_recipes:
+            os.system(f'bash {Path(__file__).parent}/recipe_info/{recipe}.sh')
+        elif recipe in py_recipes: 
+            os.system(f'python3 {Path(__file__).parent}/recipe_info/{recipe}.py')
+        else:
+            convert_recipe(recipe)
+            recipe_config = load_recipe_json(recipe)
+            archiver = Archiver(engine=os.environ['RECIPE_ENGINE'], 
+                                ftp_prefix=os.environ['FTP_PREFIX'])
+            archiver.archive_table(recipe_config)
+    except KeyError:
+        click.secho('\n Did you set your RECIPE_ENGINE and FTP_PREFIX? \n', fg='red')
